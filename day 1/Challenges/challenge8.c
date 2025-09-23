2006dcf3de64f76d42acb3455260f515b59668a9
kadiri_mohamed <med2004kad@gmail.com>
2025-09-23 17:58:25 +0100

    some changes in challenge 8 and end of challenges in day 1

--- day 1/Challenges/challenge8.c
+++ day 1/Challenges/challenge8.c
@@ -2,20 +2,32 @@
 int main(void)
 {
     float montant_pret;
-    float taux_interet = 0.06 / 12;
-    float paiment_mensuel = 386.66;
+    float x;
+    float taux_interet;
+    float paiment_mensuel;
 
     printf("donner le montant : ");
     scanf("%f", &montant_pret);
+    printf("donner le taux : ");
+    scanf("%f", &x);
+    printf("donner le paiment mensuel : ");
+    scanf("%f", &paiment_mensuel);
 
-    montant_pret = (montant_pret * taux_interet) + montant_pret - paiment_mensuel;
-    printf("Solde restant après le premier paiement : %.2f \n", montant_pret);
+    // montant_pret = (montant_pret * taux_interet) + montant_pret - paiment_mensuel;
+    // printf("Solde restant apres le premier paiement : %.2f \n", montant_pret);
 
-    montant_pret = montant_pret + taux_interet - paiment_mensuel;
-    printf("Solde restant après le deuxième paiement : %.2f \n", montant_pret);
+    // montant_pret = montant_pret + taux_interet - paiment_mensuel;
+    // printf("Solde restant apres le deuxieme paiement : %.2f \n", montant_pret);
 
-    montant_pret = montant_pret + taux_interet - paiment_mensuel;
-    printf("Solde restant après le troisième paiement : %.2f \n", montant_pret);
+    // montant_pret = montant_pret + taux_interet - paiment_mensuel;
+    // printf("Solde restant apres le troisieme paiement : %.2f \n", montant_pret);
+
+    for (int i = 0; i < 4; i++)
+    {
+        taux_interet = (x / 100) / 12;
+        montant_pret = (montant_pret * taux_interet) + montant_pret - paiment_mensuel;
+        printf("Solde restant apres le %d paiement : %.2f \n", i, montant_pret);
+    }
 
     return 0;
 }
