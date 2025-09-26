0c31b68145a524efd15ca98bb4db0718b3fa56fc
kadiri_mohamed <med2004kad@gmail.com>
2025-09-26 16:09:51 +0100

    declare avion functions and variables in headers.h

---
+++ mini projet 1/avion/headers.c
@@ -0,0 +1,19 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#define MAX 100
+
+extern int choise;
+extern int id[MAX];
+extern char modele[MAX][20];
+extern int capacite[MAX];
+extern char status[MAX][20];
+extern char date_entree[MAX][20];
+extern int counter;
+
+void ajouter_avion();
+int lister_avions();
+int modifier_avion();
+int supprimer_avion();
+int rechercher_avion();
+
+#endif
